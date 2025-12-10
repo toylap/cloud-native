45f5c2a81d4706856da490692786fe0d9381865b
최민수 <chlalstn0123@naver.com>
2025-12-10 22:47:25 +0900

    [초기설정] Cloud Native Spring In Action
    - 학습 목표 프로젝트
    - 초기 설정값 추가

---
+++ src/main/java/me/toylep/spring/cloudnative/CloudNativeApplication.java
@@ -0,0 +1,13 @@
+package me.toylep.spring.cloudnative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNativeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudNativeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/toylep/spring/cloudnative/CloudNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.toylep.spring.cloudnative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudNativeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
